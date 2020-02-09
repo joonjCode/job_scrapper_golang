abdc377fa482570501362a650725a3e625f67663
joon <jtj308@naver.com>
2020-02-10 02:03:26 +0900

    Done with Basic job scrapper using golang

--- main.go
+++ main.go
@@ -1,13 +1,30 @@
 package main
 
 import (
-	"fmt"
+	"os"
+	"strings"
 
-	"github.com/joon/learngo/accounts"
+	"github.com/joon/learngo/scrapper"
+	"github.com/labstack/echo"
 )
 
+const fileName string = "jobs.csv"
+
+func handleHome(c echo.Context) error {
+	return c.File("home.html")
+}
+func handleScrape(c echo.Context) error {
+	defer os.Remove(fileName)
+	term := strings.ToLower(scrapper.CleanString(c.FormValue("term")))
+	scrapper.Scrape(term)
+	return c.Attachment(fileName, fileName)
+}
+
 func main() {
-	account := accounts.NewAccount("joon")
-	account.Deposit(10)
-	fmt.Println(account.Balance())
+	e := echo.New()
+	e.GET("/", handleHome)
+	e.POST("/scrape", handleScrape)
+
+	e.Logger.Fatal(e.Start(":1323"))
+
 }
